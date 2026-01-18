eeb038ba597fbb27f2fd87e17f48f40ec95d2b99
brajendra <brajendrasingh3clogic@gmail.com>
2026-01-18 21:07:22 +0530

    table data updated

--- Coding/dbScript/MyTestingDbScript.sql
+++ Coding/dbScript/MyTestingDbScript.sql
@@ -15,6 +15,7 @@ CREATE TABLE employees (
     emp_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE,
     salary DECIMAL(10,2),
+    degree VARCHAR(10),	
     dept_id INT,
     manager_id INT,
     join_date DATE,
@@ -25,16 +26,18 @@ INSERT INTO departments VALUES
 (1, 'HR', 'Mumbai'),
 (2, 'IT', 'Bangalore'),
 (3, 'Finance', 'Delhi'),
-(4, 'Sales', 'Chennai');
+(4, 'Sales', 'Chennai'),
+(5, 'Operation', 'Kolkata');
 
 
 INSERT INTO employees VALUES
-(101, 'Amit', 'amit@company.com',   50000, 1, NULL, '2020-01-10'),
-(102, 'Rohit', 'rohit@company.com',  60000, 2, 105,  '2021-03-15'),
-(103, 'Neha', 'neha@company.com',   55000, 2, 105,  '2019-07-22'),
-(104, 'Pooja', 'pooja@company.com',  45000, 1, 101,  '2022-06-01'),
-(105, 'Suresh', 'suresh@company.com', 90000, 2, NULL, '2018-11-30'),
-(106, 'Kiran', 'kiran@company.com',  70000, 3, 107,  '2020-09-10'),
-(107, 'Manoj', 'manoj@company.com', 120000,3, NULL, '2017-05-19'),
-(108, 'Ravi', 'ravi@company.com',   40000, 4, 109,  '2021-12-01'),
-(109, 'Anil', 'anil@company.com',   80000, 4, NULL, '2019-08-14');
+(100, 'Shiva', 'shiva@company.com',   150000, 'B.Tech', 1, NULL, '2026-01-10'),
+(101, 'Amit', 'amit@company.com',   50000, 'M.Tech', 1, NULL, '2020-01-10'),
+(102, 'Rohit', 'rohit@company.com',  60000, 'B.Tech', 2, 105,  '2021-03-15'),
+(103, 'Neha', 'neha@company.com',   55000, 'M.Tech', 2, 105,  '2019-07-22'),
+(104, 'Pooja', 'pooja@company.com',  45000, 'B.Tech', 1, 101,  '2022-06-01'),
+(105, 'Suresh', 'suresh@company.com', 90000, 'B.Tech', 2, NULL, '2018-11-30'),
+(106, 'Kiran', 'kiran@company.com',  70000, 'M.Tech', 3, 107,  '2020-09-10'),
+(107, 'Manoj', 'manoj@company.com', 120000, 'MCA', 3, NULL, '2017-05-19'),
+(108, 'Ravi', 'ravi@company.com',   40000, 'B.Tech', 4, 109,  '2021-12-01'),
+(109, 'Anil', 'anil@company.com',   80000, 'MCA', 4, NULL, '2019-08-14');
