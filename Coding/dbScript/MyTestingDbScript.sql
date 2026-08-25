e2be2085928add4ac84b833776a845dc5f96ba90
brajendra <brajendrasingh3clogic@gmail.com>
2026-08-25 20:57:57 +0530

    db script updated

--- Coding/dbScript/MyTestingDbScript.sql
+++ Coding/dbScript/MyTestingDbScript.sql
@@ -15,6 +15,7 @@ CREATE TABLE employees (
     emp_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE,
     salary DECIMAL(10,2),
+	city VARCHAR(50),
     degree VARCHAR(15),	
     dept_id INT,
     manager_id INT,
@@ -31,15 +32,16 @@ INSERT INTO departments VALUES
 
 
 INSERT INTO employees VALUES
-(100, 'Shiva', 'shiva@company.com',   150000, 'B.Tech', 1, NULL, '2026-01-10'),
-(101, 'Amit', 'amit@company.com',   50000, 'M.Tech', 1, NULL, '2020-01-10'),
-(102, 'Rohit', 'rohit@company.com',  60000, 'B.Tech', 2, 105,  '2021-03-15'),
-(103, 'Neha', 'neha@company.com',   55000, 'M.Tech', 2, 105,  '2019-07-22'),
-(104, 'Pooja', 'pooja@company.com',  45000, 'B.Tech', 1, 101,  '2022-06-01'),
-(105, 'Suresh', 'suresh@company.com', 90000, 'B.Tech', 2, NULL, '2018-11-30'),
-(106, 'Kiran', 'kiran@company.com',  70000, 'M.Tech', 3, 107,  '2020-09-10'),
-(107, 'Manoj', 'manoj@company.com', 120000, 'MCA', 3, NULL, '2017-05-19'),
-(108, 'Ravi', 'ravi@company.com',   40000, 'B.Tech', 4, 109,  '2021-12-01'),
-(109, 'Anil', 'anil@company.com',   80000, 'MCA', 4, NULL, '2019-08-14'),
-(110, 'Rahul', 'rahul@company.com',   48000, 'MBA', 4, NULL, '2002-08-22'),
-(111, 'Ankita', 'ankita@company.com',  55000, 'Polytechnic', 1, 110,  '2025-09-01');
+(100, 'Shiva', 'shiva@company.com',   150000, 'Kanpur', 'B.Tech', 1, NULL, '2026-01-10'),
+(101, 'Amit', 'amit@company.com',   50000, 'Lucknow', 'M.Tech', 1, NULL, '2020-01-10'),
+(102, 'Rohit', 'rohit@company.com',  60000, 'Noida', 'B.Tech', 2, 105,  '2021-03-15'),
+(103, 'Neha', 'neha@company.com',   55000, 'Delhi', 'M.Tech', 2, 105,  '2019-07-22'),
+(104, 'Pooja', 'pooja@company.com',  45000, 'Mumbai', 'B.Tech', 1, 101,  '2022-06-01'),
+(105, 'Suresh', 'suresh@company.com', 90000, 'Kolkata', 'B.Tech', 2, NULL, '2018-11-30'),
+(106, 'Kiran', 'kiran@company.com',  70000, 'Chennai', 'M.Tech', 3, 107,  '2020-09-10'),
+(107, 'Manoj', 'manoj@company.com', 120000, 'Hyderabad', 'MCA', 3, NULL, '2017-05-19'),
+(108, 'Ravi', 'ravi@company.com',   40000, 'Pune', 'B.Tech', 4, 109,  '2021-12-01'),
+(109, 'Anil', 'anil@company.com',   80000, 'Kanpur', 'MCA', 4, NULL, '2019-08-14'),
+(110, 'Rahul', 'rahul@company.com',   48000, 'Delhi', 'MBA', 4, NULL, '2002-08-22'),
+(111, 'Ankita', 'ankita@company.com',  55000, 'Lucknow', 'Polytechnic', 1, 110,  '2025-09-01'),
+(112, 'Amit', 'amit2@company.com', 65000, 'Gurgaon', 'MBA', 2, 105, '2024-01-15');
